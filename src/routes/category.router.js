b00cd1357263f6c7a6a93e9e190d721c2cda29b7
gerakanamalindonesia <gerakanamalindonesia@gmail.com>
2022-06-20 14:38:13 +0700

    fixing add new category feature

--- src/routes/category.router.js
+++ src/routes/category.router.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const { getAllCategory } = require("../controllers/category.controller");
+const {
+  getAllCategory,
+  addNewCategory,
+} = require("../controllers/category.controller");
+const upload = require("../util/multer");
 const router = express.Router();
 
 router.get("/", getAllCategory);
+
+router.post("/", upload.single("image"), addNewCategory);
 
 module.exports = router;
