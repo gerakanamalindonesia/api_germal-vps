48311330f88a5f34494aeccc53c7f0b51cb51acf
gerakanamalindonesia <gerakanamalindonesia@gmail.com>
2022-06-21 17:22:42 +0700

    add detail category feature

--- src/routes/category.router.js
+++ src/routes/category.router.js
@@ -3,12 +3,14 @@ const {
   getAllCategory,
   addNewCategory,
   getCategoryWithStatus,
+  getDetailCategory,
 } = require("../controllers/category.controller");
 const upload = require("../util/multer");
 const router = express.Router();
 
 router.get("/", getAllCategory);
 router.get("/status", getCategoryWithStatus);
+router.get("/:id", getDetailCategory);
 
 router.post("/", upload.single("image"), addNewCategory);
 
